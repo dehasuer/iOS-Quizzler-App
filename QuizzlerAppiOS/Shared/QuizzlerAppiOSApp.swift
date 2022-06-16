4135c668b4bfd5f8c6bbad61e83ef02e2b711705
Deha Efecan Süer <dehasuer97@gmail.com>
2022-06-16 15:06:36 +0300

    QuizlerApp Added

---
+++ QuizzlerAppiOS/Shared/QuizzlerAppiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizzlerAppiOSApp.swift
+//  Shared
+//
+//  Created by Deha Süer on 16.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct QuizzlerAppiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
